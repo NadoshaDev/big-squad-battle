f0822efcb46b2f5396b539ba2a07634c3266d7eb
NadoshaDev <raininess@mail.ru>
2026-06-29 20:47:44 +0300

    feat: add player troop distribution

--- src/main.py
+++ src/main.py
@@ -1,16 +1,27 @@
 import random
 
-player_squads = [10, 8, 7, 9, 6]
+player_squads = []
+player_remaining = 40
+for i in range(4):
+    number_squad = i + 1
+    print(f"Формируется {number_squad} отряд")
+    quantity_warrior = int(input("Введите количество воинов в этом отряде"))
+    player_squads.append(quantity_warrior)
+    player_remaining -= quantity_warrior
+    print(f"В {number_squad} отряд добавлено {quantity_warrior} воинов")
+    print("Осталось распределить воинов:", player_remaining)
+
+print(f"В 5 отряд автоматически добавлено {player_remaining} воинов")
+player_squads.append(player_remaining)
+
 computer_squads = []
-
-remaining = 40
-
+computer_remaining = 40
 for _ in range(4):
-    random_number = random.randint(0, remaining)
+    random_number = random.randint(0, computer_remaining)
     computer_squads.append(random_number)
-    remaining -= random_number
+    computer_remaining -= random_number
 
-computer_squads.append(remaining)
+computer_squads.append(computer_remaining)
 
 player_score = 0
 computer_score = 0
